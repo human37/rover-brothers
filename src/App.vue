8deba4a15ca1b1f42f7c1ca8e2ae6233b62ef9ac
Polina Coulter <poolsrules@gmail.com>
2021-11-12 14:10:54 -0700

    header and shapes no cap

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,12 @@
         <v-spacer />
         <v-btn to="/" class="ma-4" text outlined>Home</v-btn>
         <v-spacer />
-        <div class="text-h2">SQUID GAMES</div>
+        <div class="logo">
+          <div class="shapes"><ion-icon name="ellipse-outline"></ion-icon></div>
+          <div class="shapes"><ion-icon name="triangle-outline"></ion-icon></div>
+          <div class="shapes"><ion-icon name="square-outline"></ion-icon></div>
+        </div>
+        <div class="header-text">SQUID GAMES</div>
         <v-spacer />
         <v-btn class="ma-4" to="/about" text outlined> About </v-btn>
         <v-spacer />
@@ -24,5 +29,11 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.header-text {
+  font-size: 40px;
+  color: white;
+  filter: drop-shadow(0 0 2px #ea4884) drop-shadow(0 0 2px #ea4884);
+  padding-left: 40px;
 }
 </style>
