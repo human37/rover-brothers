6f4ab2264f671368537d02a5fdcdf76ea97d09a0
Ryan Corry <d00363830@dmail.dixie.edu>
2021-11-13 00:32:54 -0700

    blac

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,24 @@
         <v-btn to="/room" class="btn" flat outlined>Create A Room</v-btn>
       </v-col>
       <v-col class="container">
-        <v-btn class="btn" flat outlined>Join A Room</v-btn>
+        <div class="join-input">
+          <v-text-field
+            class="typing-input mb-4"
+            color="#ea4884"
+            placeholder="Enter Room Code"
+            @keydown.enter="handleJoin"
+            @input="handleInput"
+          >
+          </v-text-field>
+        </div>
+        <v-btn
+          class="btn"
+          flat
+          outlined
+          @click="handleJoin"
+          style="pointer-events: none"
+          >Join</v-btn
+        >
       </v-col>
     </div>
   </div>
@@ -19,6 +36,7 @@ export default {
   },
   components: {},
   computed: {},
+  methods: {},
 };
 </script>
 
@@ -30,13 +48,13 @@ export default {
   height: 100%;
 }
 .btn {
+  position: relative;
   width: 90%;
   min-height: 100%;
   font-size: 50px;
-
   border: 1px solid;
   border-color: white;
-  box-shadow: 0px 0px 5px #ea4884,0px 0px 5px #ea4884;
+  box-shadow: 0px 0px 5px #ea4884, 0px 0px 5px #ea4884;
   color: white;
   filter: drop-shadow(0 0 2px #ea4884) drop-shadow(0 0 2px #ea4884);
 }
@@ -53,5 +71,21 @@ export default {
   align-items: center;
   height: 500px;
   width: 100%;
+}
+.join-input {
+  margin-bottom: -100px;
+  position: absolute;
+  margin-top: 25px;
+  width: 25%;
+  height: 50px;
+  color: white;
+  filter: drop-shadow(0 0 2px #ea4884) drop-shadow(0 0 2px #ea4884);
+  z-index: 1;
+}
+
+.typing-input >>> input {
+  text-align: center;
+  color: white;
+  font-size: 32px;
 }
 </style>
