185792fa25bd18c6f16acb89c77f650db1bf0950
Will Dickinson <willdickinson98@gmail.com>
2021-11-12 20:37:25 -0700

    u to the i

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,56 @@
 <template>
-  <v-app>
-    <header>
-      <v-app-bar flat outlined rounded>
-        <v-spacer />
-        <v-btn @click="$router.push('/')" class="ma-4" text outlined
-          >Home</v-btn
-        >
-        <v-spacer />
-        <div class="logo">
-          <div class="shapes"><ion-icon name="ellipse-outline"></ion-icon></div>
-          <div class="shapes">
-            <ion-icon name="triangle-outline"></ion-icon>
-          </div>
-          <div class="shapes"><ion-icon name="square-outline"></ion-icon></div>
-        </div>
-        <div class="header-text">SQUID GAMES</div>
-        <v-spacer />
-        <v-btn class="ma-4" @click="$router.push('/about')" text outlined>
-          About
-        </v-btn>
-        <v-spacer />
-
-        <v-btn icon @click="$vuetify.theme.dark = !$vuetify.theme.dark">
-          <v-icon>mdi-weather-night</v-icon>
-        </v-btn>
-      </v-app-bar>
-    </header>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+    <v-app>
+        <header>
+            <NavBar />
+        </header>
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from 'vuex';
+import NavBar from './components/NavBar.vue';
 
 export default {
-  name: 'App',
-  data: () => ({
-    tab: null,
-  }),
-  components: {
-  },
-  computed: {
-  },
-  methods: {
-    ...mapActions({
-      populateStoreData: 'populateStoreData'
+    name: 'App',
+    data: () => ({
+        tab: null,
     }),
-    connectToServer() { 
-        this.socket = new WebSocket("ws://localhost:3000");
-        this.socket.onmessage = (event) => {
-            this.receivedUpdates(event);
-        }
+    components: { NavBar },
+    computed: {},
+    methods: {
+        ...mapActions({
+            populateStoreData: 'populateStoreData',
+        }),
+        connectToServer() {
+            console.log('connecting to socket');
+            this.socket = new WebSocket(
+                `ws://${window.location.hostname}:3000`
+            );
+            this.socket.onmessage = (event) => {
+                this.receivedUpdates(event);
+            };
+        },
+        receivedUpdates(event) {
+            this.populateStoreData(JSON.parse(event.data));
+        },
+        getRoomCode() {
+            this.socket.send(JSON.stringify('GET_ROOM_CODE'));
+        },
     },
-    receivedUpdates(event) {
-      this.populateStoreData(JSON.parse(event.data));
-    }
-  },
-  created() {
-    this.connectToServer()
-  }
+    created() {
+        this.connectToServer();
+    },
 };
 </script>
 
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-.header-text {
-  font-size: 40px;
-  color: white;
-  filter: drop-shadow(0 0 2px #ea4884) drop-shadow(0 0 2px #ea4884);
-  padding-left: 40px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,74 +1,74 @@
 <template>
-  <div class="home">
-    <v-row>
-      <v-spacer />
-      <div class="prize-pool mt-6 text-center">
-        <prize-pool />
-      </div>
-      <v-spacer />
-    </v-row>
-    <v-row>
-      <v-col class="input-view ma-0 pa-0"> <TypingArea /> </v-col>
-      <v-col>
-        <div class="map-grid-view">
-          <v-btn text outlined @click="isMapView = !isMapView">{{
-            isMapViewText
-          }}</v-btn>
-          <player-map v-if="isMapView" />
-          <player-grid v-else />
-        </div>
-      </v-col>
-    </v-row>
-    <v-row>
-      <div class="container">
-        <v-btn text outlined>Submit</v-btn>
-      </div>
-    </v-row>
-  </div>
+    <div class="home">
+        <v-row>
+            <v-spacer />
+            <div class="prize-pool mt-6 text-center">
+                <prize-pool />
+            </div>
+            <v-spacer />
+        </v-row>
+        <v-row>
+            <v-col class="input-view ma-0 pa-0"> <TypingArea /> </v-col>
+            <v-col>
+                <div class="map-grid-view">
+                    <v-btn text outlined @click="isMapView = !isMapView">{{
+                        isMapViewText
+                    }}</v-btn>
+                    <player-map v-if="isMapView" />
+                    <player-grid v-else />
+                </div>
+            </v-col>
+        </v-row>
+        <v-row>
+            <div class="container">
+                <v-btn text outlined>Submit</v-btn>
+            </div>
+        </v-row>
+    </div>
 </template>
 
 <script>
-import PlayerGrid from "../components/PlayerGrid.vue";
-import PlayerMap from "../components/PlayerMap.vue";
-import TypingArea from "../components/TypingArea.vue";
-import PrizePool from "../components/PrizePool.vue";
+import PlayerGrid from '../components/PlayerGrid.vue';
+import PlayerMap from '../components/PlayerMap.vue';
+import TypingArea from '../components/TypingArea.vue';
+import PrizePool from '../components/PrizePool.vue';
 
 export default {
-  name: "Home",
-  data: function () {
-    return {
-      isMapView: false,
-    };
-  },
-  components: {
-    PlayerMap,
-    PlayerGrid,
-    TypingArea,
-    PrizePool,
-  },
-  computed: {
-    isMapViewText: function () {
-      return this.isMapView ? "Grid View" : "Map View";
+    name: 'Home',
+    data: function () {
+        return {
+            isMapView: false,
+        };
     },
-  },
+    components: {
+        PlayerMap,
+        PlayerGrid,
+        TypingArea,
+        PrizePool,
+    },
+    computed: {
+        isMapViewText: function () {
+            return this.isMapView ? 'Grid View' : 'Map View';
+        },
+    },
 };
 </script>
 
 <style scoped>
 .map-grid-view {
-  border: 1px solid #ccc;
-  border-radius: 9px;
-  margin-right: 20px;
-  margin-top: 4px;
+    border: 1px solid #ccc;
+    border-radius: 9px;
+    margin-right: 20px;
+    margin-top: 4px;
 }
 
 .input-view {
-  margin-left: 20px;
+    margin-left: 20px;
 }
 
 .container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
