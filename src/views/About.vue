a0099862ab9f227213762369edf9e2bf6aea88ad
Polina Coulter <poolsrules@gmail.com>
2021-11-12 21:48:28 -0700

    Will I don't wanna hear it no cap

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="about">
     <div class="about-form">
-      <div id="about-header">About</div>
+      <div id="about-header">Creators</div>
+      <dl class="about-list">
+        <dt>Polina Coulter</dt>
+        <dd>https://github.com/polinacoulter</dd>
+        <dt>Mark Abraham</dt>
+        <dd>https://github.com/mabraham23</dd>
+        <dt>Ammon Taylor</dt>
+        <dd>https://github.com/human37</dd>
+        <dt>Ryan Corry</dt>
+        <dd>https://github.com/rcorry</dd>
+      </dl>
     </div>
   </div>
 </template>
@@ -12,10 +22,23 @@
   border: 1px solid;
   border-color: white;
   width: 520px;
-  height: 250px;
+  height: 400px;
   margin-left: calc(50% - 250px);
   margin-top: 20px;
   box-shadow: 0px 0px 5px #ea4884,0px 0px 5px #ea4884;
+}
+.about-list dt{
+  font-size: 20px;
+  color: white;
+  filter: drop-shadow(0 0 2px #ea4884) ;
+  margin-left: 40px;
+}
+.about-list dd{
+  font-size: 20px;
+  color: white;
+  margin-left: 60px;
+  filter: drop-shadow(0 0 2px #ea4884);
+  margin-bottom: 15px;
 }
 
 #about-header {
@@ -24,7 +47,7 @@
   text-align: center;
   filter: drop-shadow(0 0 2px #ea4884) drop-shadow(0 0 2px #ea4884);
   padding-top: 20px;
-  padding-bottom: 15px;
+  padding-bottom: 10px;
 }
 
 
